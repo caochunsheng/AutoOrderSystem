2c6e462244a7dcec8028e420de61bcb3edcf21e6
caochunsheng <caochunsheng@artisman.cn>
2017-12-18 09:16:22 +0800

    添加新的UI层代码框架

---
+++ AutoOrderSystemUI/FrmBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoOrderSystem.UI
+{
+    public partial class FrmBase : Form
+    {
+        public FrmBase()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogo_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.artisman.cn/"); // 使用本机默认浏览器打开网址
+        }
+    }
+}
